c374bc7841fd7f123d7f1d14d42ef2365cfb623c
laban-kilonzo <labanmutisya7@gmail.com>
2022-09-19 09:54:06 -0700

    coding

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,22 +1,19 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * puts2 - prints every other character
- * @str: string
- *
- * Return: nothing
+ * puts2 - Prints one char out of two of a string.
+ * @str: The string containing characters.
  */
 
 void puts2(char *str)
 {
-	int i = 0;
+	int i = 0, len = 0;
 
-	while (*(str + i) != '\0')
-	{
-		if (i % 2 == 0)
-			putchar(*str + i));
-		i++;
-	}
-	putchar(10);
+	while (str[i++])
+		len++;
+
+	for (i = 0; i < len; i += 2)
+		_putchar(str[i]);
+
+	_putchar('\n');
 }
