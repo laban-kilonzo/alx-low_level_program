0413f6ce05889e1c2e0b2cc3f11f93dd8e3cd6fd
laban-kilonzo <labanmutisya7@gmail.com>
2022-09-22 07:53:05 -0700

    coding

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,8 +2,8 @@
 
 /**
  * _strcat - concatenates the string pointed to
- * by @src to the
- * end of the string pointed to by @dest
+ * by @src to the end of the string pointed to 
+ * by @dest
  * @dest: String that will be appended
  * @src: String to be concatenated upon
  *
