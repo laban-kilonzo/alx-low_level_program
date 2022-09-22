9648b935214be08d19ee85040d493f3be0c6f3d7
laban-kilonzo <labanmutisya7@gmail.com>
2022-09-22 08:37:36 -0700

    coding

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strcmp - Function which compare two strings and
+ *  _strcmp - Function which compare two strings and
  * @s1: first string
  * @s2: second string
  * Return:
