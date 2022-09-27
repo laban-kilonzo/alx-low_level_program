302709544c526a4dc5c0b841980ffb7d5923f61d
laban-kilonzo <labanmutisya7@gmail.com>
2022-09-27 08:31:54 -0700

    coding

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,17 +10,25 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i, j;
+	int i, j, f;
 
-	for (i = 0; s[i]; i++)
+	i = 0;
+	while (s[i] != '\0')
 	{
-		for (j = 0; accept[j]; j++)
+		j = 0;
+		f = 1; /*flag status*/
+		while (accept[j] != '\0')
 		{
 			if (s[i] == accept[j])
+			{
+				f = 0; /*success*/
 				break;
+			}
+			j++
 		}
-		if (!accept[j])
+		if (f == 1)
 			break;
 	}
-	return (i);
+	i++;
 }
+
