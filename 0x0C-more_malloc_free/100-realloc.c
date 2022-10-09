24fde65d45d7802553c7c5edc801cba22fec677d
laban-kilonzo <labanmutisya7@gmail.com>
2022-10-09 07:45:27 -0700

    coding

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -13,8 +13,7 @@
  * or NULL if failure
  */
 
-void *_realloc(void *ptr, unsigned int old_size, 
-unsigned int new_size)
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	char *p;
 	unsigned int i, max = new_size;
