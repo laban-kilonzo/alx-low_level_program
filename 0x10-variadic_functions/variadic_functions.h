04b9b2cb0cad94beefe2246bdf18263ba24a0a74
laban-kilonzo <labanmutisya7@gmail.com>
2022-10-14 02:45:28 -0700

    coding

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -22,6 +22,6 @@ void print_numbers(const char *separator,
 		const unsigned int n, ...);
 void print_strings(const char *separator,
 		const unsigned int n, ...);
-void print_all(const char * const format, ...);
+void print_all(const char *const format, ...);
 
 #endif /* _VARIADIC_FUNCTIONS_H_ */
